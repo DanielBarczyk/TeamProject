f8b38f599fcb8e0c28011814ba748e4f43fcaa11
mrzigod <mrzigod@gmail.com>
2022-04-03 19:53:24 +0200

    jumpboi

--- Assets/Scenes/NewBehaviourScript.cs
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    bool isJumping;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isJumping=false;
     }
 
     // Update is called once per frame
@@ -23,8 +24,18 @@ public class NewBehaviourScript : MonoBehaviour
 
         float mouseXvalue =Input.GetAxis("Mouse X");
         float mouseYvalue =Input.GetAxis("Mouse Y");
-        Quaternion deltaRotation = Quaternion.Euler(new Vector3(-1*mouseYvalue,mouseXvalue,0));
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(0,mouseXvalue,0));
         rb.MoveRotation(rb.rotation*deltaRotation);
+
+        if(Input.GetKey("space")){
+            if(isJumping==false){
+                isJumping=true;
+                rb.AddForce(new Vector3(0,100,0));
+            }
+        }
+        if(rb.velocity.y==0){
+            isJumping=false;
+        }
     }
 
 }

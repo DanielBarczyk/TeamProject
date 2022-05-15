a3d6cae97b87c1797150d34c5e6167d81142f9f4
mrzigod <mrzigod@gmail.com>
2022-05-15 18:26:53 +0200

    Picking up items with E

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -26,28 +26,43 @@ public class Inventory : MonoBehaviour
         }
     }
 
-    public GameObject cubeCount;
+    public class Item
+    {
+        public string name;
+        public Item()
+        {
+            name="New Item";
+        }
+        public Item(string name)
+        {
+            this.name=name;
+        }
+    }
+
     public GameObject inventoryScreen;
 
     public GameObject currentWeapon;
 
     public GameObject allWeapons;
 
+    public GameObject inventoryItems;
+
     bool visible;
     int cubecount;
 
     List<Weapon> weapons;
+    List<Item> items;
     public Weapon equippedWeapon;
 
     void Start(){
         visible=false;
         inventoryScreen.SetActive(false);
-        cubeCount.GetComponent<Text>().text="Cubes Collected:0";
-        cubecount=0;
         equippedWeapon=new Weapon();
         weapons=new List<Weapon>();
         weapons.Add(equippedWeapon);
         AddWeapon("Strong and Slow",10,420);
+        items=new List<Item>();
+        UpdateItems();
     }
     void Update()
     {
@@ -66,22 +81,15 @@ public class Inventory : MonoBehaviour
             }
         }
     }
-
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Collected");
-        cubecount++;
-        cubeCount.GetComponent<Text>().text="Cubes Collected:"+cubecount.ToString();
-    }
-
     void AddWeapon(string name, int atk, int delay){
-        weapons.Add(new Weapon(name,atk,delay));
+        if(weapons.Count<9)
+            weapons.Add(new Weapon(name,atk,delay));
         UpdateWeapons();
     }
     void UpdateWeapons()
     {
         currentWeapon.GetComponent<Text>().text="Current Weapon:\n"+equippedWeapon.name+"\nAttack: "+equippedWeapon.atk+"\ndelay: "+equippedWeapon.shotDelay;
-        allWeapons.GetComponent<Text>().text="All Weapons:\n";
+        allWeapons.GetComponent<Text>().text="All Weapons: "+weapons.Count+"/9\n";
         foreach(var val in weapons)
         {
             allWeapons.GetComponent<Text>().text+=val.name+"\nAttack: "+val.atk+"\ndelay: "+val.shotDelay+"\n\n";
@@ -93,6 +101,26 @@ public class Inventory : MonoBehaviour
         {
             equippedWeapon=weapons[arg-1];
             UpdateWeapons();
+        }
+    }
+
+    public bool AddItem(string name)
+    {
+        if(items.Count<40)
+        {
+            items.Add(new Item(name));
+            UpdateItems();
+            return true;
+        }
+        return false;
+    }
+
+    void UpdateItems()
+    {
+        inventoryItems.GetComponent<Text>().text="Current Items:\n";
+        foreach(var val in items)
+        {
+            inventoryItems.GetComponent<Text>().text+=val.name+"\n";
         }
     }
 }
